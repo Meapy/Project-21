9e41d09d4e1d7945b33522182c2f2ccccf06fae7
Meapy <dkrasovski.20@gmail.com>
2020-02-16 00:01:40 +0000

    character select mostly done

--- Assets/Scripts/CharacterSelection.cs
+++ Assets/Scripts/CharacterSelection.cs
@@ -1,18 +1,70 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+using UnityEngine.SceneManagement;
 
 public class CharacterSelection : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    private int selectedCharacterIndex;
+    private Color desiredColor;
+
+    [Header("List of characters")]
+    [SerializeField] private List<CharacterSelectObject> characterList = new List<CharacterSelectObject>();
+
+    [Header("UI References")]
+    [SerializeField] private TextMeshProUGUI characterName;
+    [SerializeField] private Image characterSplash;
+    [SerializeField] private Image backgroundColor;
+
+    [Header("Sounds")]
+    [SerializeField] private AudioClip arrowclicksfx;
+    [SerializeField] private AudioClip characterSelectMusic;
+    private void start()
     {
-        
+        UpdateCharacterSelectionUI();
     }
 
-    // Update is called once per frame
-    void Update()
+    public void LeftArrow()
     {
-        
+        selectedCharacterIndex--;
+        if(selectedCharacterIndex < 0)
+        {
+            selectedCharacterIndex = characterList.Count - 1;
+        }
+        UpdateCharacterSelectionUI();
+    }
+
+    public void RightArrow()
+    {
+        selectedCharacterIndex++;
+        if (selectedCharacterIndex == characterList.Count)
+        {
+            selectedCharacterIndex = 0;
+        }
+        UpdateCharacterSelectionUI();
+    }
+
+    public void Confirm()
+    {
+        Debug.Log(string.Format("Character{0}:{1} has been chosen",selectedCharacterIndex,characterList[selectedCharacterIndex].characterName));
+        SceneManager.LoadScene("LevelSelect");
+    }
+
+    private void UpdateCharacterSelectionUI()
+    {
+        characterSplash.sprite = characterList[selectedCharacterIndex].splash;
+        characterName.text = characterList[selectedCharacterIndex].characterName;
+        desiredColor = characterList[selectedCharacterIndex].characterColor;
+    }
+
+    [System.Serializable]
+    public class CharacterSelectObject
+    {
+        public Sprite splash;
+        public string characterName;
+        public Color characterColor;
+         
     }
 }
