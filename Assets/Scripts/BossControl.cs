69b2e0dadcfcf6fc72e870d9938d1ff28e85084f
NikolayMal <55692364+NikolayMal@users.noreply.github.com>
2020-02-18 18:24:49 +0000

    Boss Complete
    He now is only activated from a certain point set in unity ; his movement & shooting

--- Assets/Scripts/BossControl.cs
+++ Assets/Scripts/BossControl.cs
@@ -8,6 +8,7 @@ public class BossControl : MonoBehaviour
     public float speed;
     public float stoppingDistance;
     public float retreatDistance;
+    public float startDistance;
     public float startTimeBetweenShots;
     private float timeBetweenShots;
 
@@ -25,36 +26,40 @@ public class BossControl : MonoBehaviour
 
     void Update()
     {
-        bossHealthBar.value = health;
-
-        if (Vector2.Distance(transform.position, player.position) > stoppingDistance) 
-        { // Moves towards player if the boss is too far away
-            transform.position = Vector2.MoveTowards(transform.position, player.position, speed * Time.deltaTime);
-        }
-        else if(Vector2.Distance(transform.position, player.position) < stoppingDistance && Vector2.Distance(transform.position, player.position) > retreatDistance)
-        { // Stays in the same spot if the player is within the distance parameters
-            transform.position = this.transform.position;
-        }  
-        else if(Vector2.Distance(transform.position, player.position) < retreatDistance)
-        { // Moves backwards if the player moves too close
-            transform.position = Vector2.MoveTowards(transform.position, player.position, -speed * Time.deltaTime);
-        }
-
-        // Boss Shooting
-        if (timeBetweenShots <= 0)
+        if (player != null) // once player dies we good
         {
-            Instantiate(BossProjectile1, transform.position, Quaternion.identity); // creates projectile from BossProjectile1
-            timeBetweenShots = startTimeBetweenShots; // Limits boss to shooting only a certain amount of times
-        }
-        else
-        {
-            timeBetweenShots -= Time.deltaTime; // Counts down timer before boss can shoot again
-        }
+            bossHealthBar.value = health;
 
-        if (health <= 0)
-        {
-            Instantiate(deathEffect, transform.position, Quaternion.identity); 
-            Destroy(gameObject);
+            if (Vector2.Distance(transform.position, player.position) > stoppingDistance && Vector2.Distance(transform.position, player.position) < startDistance)
+            { // Moves towards player if the boss is too far away
+                transform.position = Vector2.MoveTowards(transform.position, player.position, speed * Time.deltaTime);
+            }
+            else if (Vector2.Distance(transform.position, player.position) < stoppingDistance && Vector2.Distance(transform.position, player.position) > retreatDistance && Vector2.Distance(transform.position, player.position) < startDistance)
+            { // Stays in the same spot if the player is within the distance parameters
+                transform.position = this.transform.position;
+            }
+            else if (Vector2.Distance(transform.position, player.position) < retreatDistance && Vector2.Distance(transform.position, player.position) < startDistance)
+            { // Moves backwards if the player moves too close
+                transform.position = Vector2.MoveTowards(transform.position, player.position, -speed * Time.deltaTime);
+            }
+
+            // Boss Shooting
+            if (timeBetweenShots <= 0 && Vector2.Distance(transform.position, player.position) < startDistance)
+            {
+                Instantiate(BossProjectile1, transform.position, Quaternion.identity); // creates projectile from BossProjectile1
+                timeBetweenShots = startTimeBetweenShots; // Limits boss to shooting only a certain amount of times
+            }
+            else
+            {
+                timeBetweenShots -= Time.deltaTime; // Counts down timer before boss can shoot again
+            }
+
+            if (health <= 0)
+            {
+                Instantiate(deathEffect, transform.position, Quaternion.identity);
+                Destroy(gameObject);
+               
+            }
         }
     }
 
