2d30a614faab975576ab0b5fb00ae5a2c15a0c03
kxngbari <ireadebari@gmail.com>
2020-02-22 23:04:31 +0000

    Settings
    just another likkle updateting

--- Assets/SettingsMenu.cs
+++ Assets/SettingsMenu.cs
@@ -5,9 +5,9 @@ using UnityEngine.Audio;
 
 public class SettingsMenu : MonoBehaviour
 {
-    
-    public AudioMixer audioMixer;
 
+    public AudioMixer audioMixer;
+    
     public void SetVolume (float volume)
     {
         audioMixer.SetFloat("volume", volume);
@@ -16,11 +16,6 @@ public class SettingsMenu : MonoBehaviour
     public void SetQuality (int qualityIndex)
     {
         QualitySettings.SetQualityLevel(qualityIndex);
-    }
-
-    public void SetFullscreen (bool isFullscreen)
-    {
-        Screen.fullScreen = isFullscreen;
     }
 
 }
