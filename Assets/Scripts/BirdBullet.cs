1cfaf077834d9ff499fc7bbb672faf8594032f72
NikolayMal <55692364+NikolayMal@users.noreply.github.com>
2020-02-22 18:05:07 +0000

    BirdBoss
    Bird Boss shooting

---
+++ Assets/Scripts/BirdBullet.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BirdBullet : MonoBehaviour
+{
+    private Vector2 moveDirection;
+    public float bulletSpeed;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+            
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Translate(moveDirection * bulletSpeed * Time.deltaTime);
+    }
+
+    public void SetMoveDirection(Vector2 dir)
+    {
+        moveDirection = dir;
+    }
+
+    private void OnDestroy()
+    {
+        gameObject.SetActive(false);
+    }
+
+    private void OnDisable()
+    {
+        CancelInvoke();        
+    }
+}
---
+++ Assets/Scripts/BirdMovement.cs
@@ -0,0 +1,67 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class BirdMovement : MonoBehaviour
+{
+    public float moveSpeed;
+    private bool moveRight;
+
+    public Slider bossHealthBar;
+    public float health;
+    public GameObject deathEffect;
+
+    private Transform player;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        moveRight = true;
+        player = GameObject.FindGameObjectWithTag("Player").transform;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        bossHealthBar.value = health;
+
+        if (player != null)
+        {
+
+            if (transform.position.x > 2f)
+            {
+                moveRight = false;
+                transform.localRotation = Quaternion.Euler(0, 180, 0);
+            }
+            else if (transform.position.x < -2f)
+            {
+                moveRight = true;
+                transform.localRotation = Quaternion.Euler(0, 0, 0);
+            }
+
+            if (moveRight)
+            {
+                transform.position = new Vector2(transform.position.x + moveSpeed * Time.deltaTime, transform.position.y);
+
+            }
+            else
+            {
+                transform.position = new Vector2(transform.position.x - moveSpeed * Time.deltaTime, transform.position.y);
+            }
+
+            if (health <= 0)
+            {
+                //Instantiate(deathEffect, transform.position, Quaternion.identity);
+                Destroy(gameObject);
+
+            }
+        }
+    }
+
+    public void TakeDamage(int damage)
+    {
+        health -= damage;
+        Debug.Log("Boss Health is " + health);
+    }
+}
