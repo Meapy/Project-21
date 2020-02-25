5987a3a52d3b468907789dbf33fdedc7cf1fef3f
Daniel Krasovski <dkrasovskijj@gmail.com>
2020-02-25 15:53:16 +0000

    update

--- Assets/Scripts/Score.cs
+++ Assets/Scripts/Score.cs
@@ -2,26 +2,33 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Score : MonoBehaviour
-{
-
-    public int totalScore;
-    public int maxtime = 360;
-    public int BossScore = 500;
-
-    totalScore = maxtime + BossScore;
-
-
-    
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
-}
+//public class Score : MonoBehaviour
+//{
+//
+//    public int totalScore;
+//    public int maxtime = 360;
+//    public int BossScore = 500;
+//
+//    
+//
+//
+//    
+//    // Start is called before the first frame update
+//    void Start()
+//    {
+//        
+//    }
+//
+//    // Update is called once per frame
+//    void Update()
+//    {
+//        
+//    }
+//
+//    public void ScoringSystem()
+//    {
+//
+//
+//        totalScore = maxtime + BossScore
+//    }
+//}
