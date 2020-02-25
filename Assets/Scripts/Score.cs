93f429e286aae4a6925ae424b91cfa789cb4164b
Daniel Krasovski <dkrasovskijj@gmail.com>
2020-02-25 16:59:07 +0000

    more score work

--- Assets/Scripts/Score.cs
+++ Assets/Scripts/Score.cs
@@ -1,6 +1,8 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
 
 public class Score : MonoBehaviour
 {
@@ -8,15 +10,21 @@ public class Score : MonoBehaviour
     public int totalScore;
     public int maxtime = 360;
     public int BossScore = 500;
+    public int timeleft;
 
     public Transform player;
+    public TextMeshProUGUI scoreText;
 
 
     
-
     // Update is called once per frame
     void Update()
     {
-        Debug.Log(player.position.z);
+        for(int i = maxtime * 60; i > 0; i--)
+        {
+            timeleft = i;
+        }
+        scoreText.text = 0.ToString();
+        scoreText.text = totalScore.ToString() + timeleft.ToString();
     }
 }
