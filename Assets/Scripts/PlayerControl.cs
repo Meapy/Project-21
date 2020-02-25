cf09859743374c21fb2bec31c0bea1e8742d4814
kxngbari <ireadebari@gmail.com>
2020-02-25 15:43:42 +0000

    Revert "update"
    This reverts commit d86865a83f7bed66917d6ab1684fbf44715d5b3c, reversing
    changes made to c37222070d2d5767c6ff9dce2f6a248c9bc29f26.

--- Assets/Scripts/PlayerControl.cs
+++ Assets/Scripts/PlayerControl.cs
@@ -10,20 +10,21 @@ public class PlayerControl : MonoBehaviour
     Vector2 movement;
 
     public Animator animator;
+
+    /*
     public float health;
     public Slider playerHealthBar;
     public Transform enemy;
     public GameObject deathEffect;
     public GameObject hitEffect;
 
-
     void Start()
     {
-     //   anim = GetComponent<Animator>();
+        anim = GetComponent<Animator>();
         rb = GetComponent<Rigidbody2D>();
         enemy = GameObject.FindGameObjectWithTag("Enemy").transform;
     }
-    
+    */
     void Update()
     {
         movement.x = Input.GetAxisRaw("Horizontal");
@@ -33,7 +34,7 @@ public class PlayerControl : MonoBehaviour
         animator.SetFloat("Vertical", movement.y);
         animator.SetFloat("Speed", movement.magnitude);
 
-        
+        /*
         playerHealthBar.value = health;
 
         if (health <= 0)
@@ -41,22 +42,7 @@ public class PlayerControl : MonoBehaviour
             Instantiate(deathEffect, transform.position, Quaternion.identity);
             Destroy(gameObject);
         }
-        
-    }
-
-    public void TakeDamage(int damage)
-    {
-        health -= damage;
-        Debug.Log("Player Health is " + health);
-    }
-
-    public void OnTriggerEnter2D(Collider2D other)
-    {
-        Debug.Log("Hit Detected");
-        GameObject e = Instantiate(hitEffect) as GameObject;
-        e.transform.position = transform.position;
-        other.gameObject.SetActive(false);
-        TakeDamage(10);
+        */
     }
 
 
