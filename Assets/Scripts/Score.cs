ec9d16115569e01275e970f925ab1669575926b5
Daniel Krasovski <dkrasovskijj@gmail.com>
2020-02-25 18:28:16 +0000

    time part of score complete

--- Assets/Scripts/Score.cs
+++ Assets/Scripts/Score.cs
@@ -11,20 +11,30 @@ public class Score : MonoBehaviour
     public int maxtime = 360;
     public int BossScore = 500;
     public int timeleft;
+    public int startTime;
 
     public Transform player;
     public TextMeshProUGUI scoreText;
 
 
-    
+    void Start()
+    {
+        startTime = maxtime * 60;
+    }
+
     // Update is called once per frame
     void Update()
     {
-        for(int i = maxtime * 60; i > 0; i--)
-        {
-            timeleft = i;
-        }
-        scoreText.text = 0.ToString();
-        scoreText.text = totalScore.ToString() + timeleft.ToString();
+
+        getScore();
+        
+    }
+
+    void getScore()
+    {
+        startTime--;
+        timeleft = startTime;
+        totalScore = (timeleft + BossScore) / 60;
+        scoreText.text = totalScore.ToString();
     }
 }
