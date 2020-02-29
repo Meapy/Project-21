8cdaa262552bb6032939a22701581564eeabddf1
Meapy <dkrasovski.20@gmail.com>
2020-02-29 20:25:31 +0000

    fixed overlapping music

--- Assets/Scripts/Sound.cs
+++ Assets/Scripts/Sound.cs
@@ -1,14 +1,24 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class Sound : MonoBehaviour
 {
     private static Sound instance;
+    private int menuCheck;
+    public string sceneName;
+    public string previoussceneName;
 
     private void Start()
     {
         Score.level = 0;
+        Scene currentScene = SceneManager.GetActiveScene();
+        sceneName = currentScene.name;
+        if (sceneName == "MainMenu" )
+        {
+            menuCheck = 0;
+        }
     }
     public static Sound Instance 
     
@@ -19,13 +29,28 @@ public class Sound : MonoBehaviour
 
     private void Update()
     {
-        for(int i = 1; i <= 3; i++)
+        Scene currentScene = SceneManager.GetActiveScene();
+        sceneName = currentScene.name;
+        if(sceneName == "LevelSelect" || sceneName == "SettingsMenu")
+        {
+            previoussceneName = "MainMenu";
+        }
+        if(sceneName == "MainMenu" && previoussceneName == "MainMenu")
+        {
+            Destroy(gameObject);
+        }
+        for (int i = 1; i <= 3; i++)
         {
             if (Score.level == i)
             {
                 Destroy(gameObject);
             }
         }
+        if (menuCheck == 1)
+        {
+             Destroy(gameObject);
+        }
+
     }
     void Awake()
     {
@@ -34,8 +59,7 @@ public class Sound : MonoBehaviour
             instance = this;
             DontDestroyOnLoad(instance);
         }
-
-        
+ 
     }
 }
 
